5d557c0adf834a96bff559189d296a74fba6152e
Shrikant Patil <pailshrikant77@gmail.com>
2024-03-19 10:05:06 +0530

    initial commit

--- App/AuthConfig.ts
+++ App/AuthConfig.ts
@@ -1,15 +1,15 @@
 import { Configuration, PopupRequest } from "@azure/msal-browser";
 
 // Config Azure AD app setting to be passed to Msal on creation
-const TenantId = "11111111-1111-1111-1111-111111111111";
-const ClientId = "22222222-2222-2222-2222-222222222222";
+const TenantId = "9a39e593-8f37-4c2c-9ab8-71e7efe6a0ac";
+const ClientId = "665de04b-89ce-4d9e-9cb7-e47b3d766072";
 
 export const msalConfig: Configuration = {
     auth: {
         clientId: ClientId,
         authority: "https://login.microsoftonline.com/" + TenantId,
         redirectUri: "/",
-        postLogoutRedirectUri: "/"        
+        postLogoutRedirectUri: "/"
     }
 };
 
@@ -20,7 +20,7 @@ export const PowerBiPermissionScopes: string[] = [
     "https://analysis.windows.net/powerbi/api/Group.Read.All",
     "https://analysis.windows.net/powerbi/api/Workspace.ReadWrite.All",
     "https://analysis.windows.net/powerbi/api/Content.Create"
-  ]
+]
 
 export const PowerBiLoginRequest: PopupRequest = {
     scopes: PowerBiPermissionScopes
